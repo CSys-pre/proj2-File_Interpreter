f5582a711369dced8ed2ae6c117ad217a7f3f629
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-07-07 22:29:14 +0200

    create a new node - function

--- stack.c
+++ stack.c
@@ -1,9 +1,23 @@
 #include "stack.h"
+#include <assert.h>
+#include <stdlib.h>
+
+static Stack_Node* start_node;
+static Stack_Node* end_node;
 
 // acts like constructor
 void Stack_init()
 {
+    start_node = NULL;
+    end_node   = NULL;
+}
 
+Stack_Node* Stack_create_node()
+{
+    Stack_Node* node = malloc( sizeof(Stack_Node) );
+    node->next = NULL;
+
+    return node;
 }
 
 // acts like destructor
