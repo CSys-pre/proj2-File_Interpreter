bbb250ea1377b4ef7e4032646d6643265c91fe07
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-07-07 22:28:46 +0200

    add functions prototypes

--- stack.h
+++ stack.h
@@ -4,8 +4,18 @@
 /* this an implementation of simple Stack in C
  */
 
+typedef struct Stack_Node {
+    char data;
+    struct Stack_Node* next;
+} Stack_Node;
+
 // acts like constructor
 void Stack_init();
+
+Stack_Node* Stack_create_node();
+void Stack_push( Stack_Node* node );
+Stack_Node* Stack_pop();
+
 // acts like destructor
 void Stack_clean();
 
