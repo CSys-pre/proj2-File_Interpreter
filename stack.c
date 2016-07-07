e9db93f34b616911958b50c3a188502fdf750f1f
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-07-07 22:08:42 +0200

    create `stack` library

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "stack.h"
+
+// acts like constructor
+void Stack_init()
+{
+
+}
+
+// acts like destructor
+void Stack_clean()
+{
+
+}
---
+++ stack.h
@@ -0,0 +1,12 @@
+#ifndef STACK_H
+#define STACK_H
+
+/* this an implementation of simple Stack in C
+ */
+
+// acts like constructor
+void Stack_init();
+// acts like destructor
+void Stack_clean();
+
+#endif // STACK_H
