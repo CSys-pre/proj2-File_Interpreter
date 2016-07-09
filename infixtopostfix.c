257cf80f68f065058ca7d6282b45da14d2a135e4
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-07-09 03:05:14 +0200

    create `string_to_integer` function

--- infixtopostfix.c
+++ infixtopostfix.c
@@ -3,3 +3,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+static uint32_t string_to_integer( const char* str );
+
+uint32_t string_to_integer( const char* str )
+{
+    size_t len = strlen( str );
+    uint32_t integer = 0;
+    uint32_t tens = 1;
+
+    for (int iii = len - 1; iii >= 0; --iii)
+    {
+        integer += (uint32_t)(str[iii] - '0') * tens;
+        tens *= 10;
+    }
+
+    return integer;
+}
