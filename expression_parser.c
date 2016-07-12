4ad04607601f63544c6c78835b8368a5567ff39e
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-07-12 23:06:32 +0200

    - create a `parser` function that take a mathematical expression as a string and return the result as number - create a function that check for correct initialization of this library - create another ascii table to hold the pointer of functions that represent operators - reconstruct infix_to_postfix_inner - now it handle extra spaces in infix - create a function that remove spaces from a string. - string_to_integer now takes the length as a parameter. - add some documentaions

--- expression_parser.c
+++ expression_parser.c
@@ -1,29 +1,48 @@
 #include "expression_parser.h"
 #include "stack.h"
-
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 
 #define MAX_POSTFIX_SIZE 100
 
-static uint32_t string_to_integer( const char* str );
+static uint32_t string_to_integer( const char* str, size_t len );
 static uint32_t ExpressionParser_infix_to_postfix_inner(const char *infix, char* postfix );
+static char* remove_spaces( char* str );
+static bool check_initation();
 
-static const int8_t* ASCII = NULL;
+static uint32_t __add( uint32_t num1, uint32_t num2 );
+static uint32_t __sub( uint32_t num1, uint32_t num2 );
+static uint32_t __mul( uint32_t num1, uint32_t num2 );
+static uint32_t __div( uint32_t num1, uint32_t num2 );
+static uint32_t __mod( uint32_t num1, uint32_t num2 );
+static uint32_t __shift_left( uint32_t num1, uint32_t num2 );
+static uint32_t __shift_right( uint32_t num1, uint32_t num2 );
+static uint32_t __and( uint32_t num1, uint32_t num2 );
+static uint32_t __or( uint32_t num1, uint32_t num2 );
+static uint32_t __xor( uint32_t num1, uint32_t num2 );
+
+// type def to this pointer of function with name `ASCII_FUNC_PTR`
+typedef uint32_t (*ASCII_FUNC_PTR)(uint32_t, uint32_t);
+
+static const int8_t* ASCII_PRI = NULL;
+static const ASCII_FUNC_PTR* ASCII_ADDR_ARR;
 static const uint16_t ASCII_LEN = 256;
 
 void ExpressionParser_init()
 {
     // to make sure that this function called once
-    if( ASCII != NULL )
+    if( ASCII_PRI != NULL )
         return;
 
     // Create ASCII table
     // we use temp here as a temporary pointer to assign the values
     // then make the table const ( by make the pointer hold it const )
     int8_t* temp = malloc( ASCII_LEN * sizeof(int8_t) );
+    ASCII_FUNC_PTR* temp2 = malloc( ASCII_LEN * sizeof(ASCII_FUNC_PTR) );
 
+    // TODO: we need to remove that part - performance
     // assign all its members by -1 by default
     for (int iii = 0; iii < ASCII_LEN; ++iii)
     {
@@ -34,29 +53,49 @@ void ExpressionParser_init()
     // according to their precedence ( 0 means highest priority )
     temp['('] = 0;
     temp[')'] = 0;
-
     temp['*'] = 1;
     temp['/'] = 1;
     temp['%'] = 1;
-
     temp['+'] = 2;
     temp['-'] = 2;
-
     temp['>'] = 3;
     temp['<'] = 3;
-
     temp['&'] = 4;
-
     temp['^'] = 5;
-
     temp['|'] = 6;
 
-    ASCII = ( const int8_t* )temp;
+    // pointer of functions for operators
+    temp2['*'] = __mul;
+    temp2['/'] = __div;
+    temp2['%'] = __mod;
+    temp2['+'] = __add;
+    temp2['-'] = __sub;
+    temp2['>'] = __shift_right;
+    temp2['<'] = __shift_left;
+    temp2['&'] = __and;
+    temp2['^'] = __xor;
+    temp2['|'] = __or;
+
+    ASCII_PRI = ( const int8_t* )temp;
+    ASCII_ADDR_ARR = ( const ASCII_FUNC_PTR* )temp2;
 }
 
-uint32_t string_to_integer( const char* str )
+bool check_initation()
 {
-    size_t len = strlen( str );
+    // to get sure that that `init` has been called
+    if( ASCII_PRI == NULL )
+    {
+        #ifdef __DEBUG__
+            fprintf(stderr, "`ExpressionParser_init()` function has not been called probably\n");
+        #endif
+            return false;
+    }
+
+    return true;
+}
+
+uint32_t string_to_integer( const char* str, size_t len )
+{
     uint32_t integer = 0;
     uint32_t tens = 1;
 
@@ -71,15 +110,14 @@ uint32_t string_to_integer( const char* str )
 
 char* ExpressionParser_infix_to_postfix( const char* infix )
 {
-    // to get sure that
-    assert( ASCII != NULL );
+    if( check_initation() == false )
+        return NULL;
+
+    infix = remove_spaces( (char *)infix );
 
     char *postfix = malloc(MAX_POSTFIX_SIZE * sizeof(char));
 
-    size_t postfix_index = ExpressionParser_infix_to_postfix_inner( infix, postfix );
-
-    // add the NULL termianal to the end of the postfix
-    postfix[postfix_index] = '\0';
+    ExpressionParser_infix_to_postfix_inner( infix, postfix );
 
     return postfix;
 }
@@ -88,30 +126,29 @@ uint32_t ExpressionParser_infix_to_postfix_inner(const char *infix, char* postfi
 {
     size_t len;
 
-    if( strcmp(infix,  "" ) != 0 )
+    if( strcmp(infix, "" ) != 0 )
         len = strlen(infix);
 
     else
         return 0;
 
-    bool is_operand = false;
-
     Stack* stack = Stack_new();
 
     uint32_t postfix_index = 0;
 
-    for (uint32_t iii = 0; iii < len; ++iii)
+    uint32_t index = 0;
+
+    while ( index < len )
     {
         // Operand
-        if( infix[iii] >= '0' && infix[iii] <= '9' )
+        if( infix[index] >= '0' && infix[index] <= '9' )
         {
-            postfix[postfix_index++] = infix[iii];
+            while( infix[index] >= '0' && infix[index] <= '9' )
+            {
+                postfix[postfix_index++] = infix[index++];
+            }
 
-            // as a number could consists of more that one digit
-            if( is_operand == true )
-                postfix[postfix_index++] = ' ';
-
-            is_operand = true;
+            postfix[postfix_index++] = ' ';
         }
 
         // Opeators
@@ -119,26 +156,26 @@ uint32_t ExpressionParser_infix_to_postfix_inner(const char *infix, char* postfi
         {
             if( Stack_is_empty(stack) == false )
             {
-                uint8_t stack_last_element_priority = ASCII[(uint8_t)Stack_get_last_char(stack)];
-                uint8_t current_operand_priority    = ASCII[(uint8_t)infix[iii]];
+                uint8_t stack_last_element_priority = ASCII_PRI[(uint8_t)Stack_get_last_char(stack)];
+                uint8_t current_operand_priority    = ASCII_PRI[(uint8_t)infix[index]];
 
                 // handle inner infix - the one between `()`
-                if( infix[iii] == '(' )
+                if( infix[index] == '(' )
                 {
                     char *sub_infix = malloc( MAX_POSTFIX_SIZE * sizeof(char) );
-                    uint32_t index = 0;
+                    uint32_t index2 = 0;
 
                     // ignore '('
-                    iii++;
+                    index++;
 
                     // gather the data between the `()`
-                    while( infix[iii] != ')' )
+                    while( infix[index] != ')' )
                     {
-                        sub_infix[index++] = infix[iii++];
+                        sub_infix[index2++] = infix[index++];
                     }
 
                     // ignore ')'
-                    iii++;
+                    index++;
 
                     // the data between `()` is calculated as a new infix
                     // update the postfix_index
@@ -154,26 +191,29 @@ uint32_t ExpressionParser_infix_to_postfix_inner(const char *infix, char* postfi
                 // note: ASCII priority is inverted ( 0 has the highest priority )
                 while( stack_last_element_priority <= current_operand_priority )
                 {
-                    // add space after each operand poped out from the stack
-                    postfix[postfix_index++] = ' ';
                     // push operator
                     postfix[postfix_index++] = Stack_pop_char( stack );
 
-                    stack_last_element_priority = ASCII[(uint8_t)Stack_get_last_char(stack)];
-                    current_operand_priority    = ASCII[(uint8_t)infix[iii]];
+                    // add space after each operand poped out from the stack
+                    postfix[postfix_index++] = ' ';
+
+                    // TODO:
+                    if( Stack_is_empty(stack) == true )
+                        break;
+
+                    stack_last_element_priority = ASCII_PRI[(uint8_t)Stack_get_last_char(stack)];
+                    current_operand_priority    = ASCII_PRI[(uint8_t)infix[index]];
                 }
             }
 
             // push operator
-            Stack_push_char(stack, infix[iii]);
+            Stack_push_char(stack, infix[index]);
 
-            // add space after each operand
-            postfix[postfix_index++] = ' ';
-
-            is_operand = false;
+            index++;
         }
     }
 
+    // may be there are some operators still on the stack
     while( Stack_is_empty(stack) == false )
     {
         // push operator
@@ -183,11 +223,149 @@ uint32_t ExpressionParser_infix_to_postfix_inner(const char *infix, char* postfi
         postfix[postfix_index++] = ' ';
     }
 
+    // add the NULL termianal to the end of the postfix
+    postfix[postfix_index] = '\0';
+
     return postfix_index;
+}
+
+uint32_t ExpressionParser_parse( const char* infix )
+{
+    if(check_initation() == false)
+    {
+        return -1;
+    }
+
+    uint32_t result = 0;
+
+    char* postfix = malloc( MAX_POSTFIX_SIZE * sizeof(char) );
+    postfix = ExpressionParser_infix_to_postfix( infix );
+    size_t postfix_len = strlen( postfix );
+
+    Stack* stack = Stack_new();
+
+    uint32_t index = 0;
+
+    char* token;
+    size_t token_len;
+
+    while ( index < postfix_len )
+    {
+        token = &postfix[index];
+        token_len = 0;
+
+        while( postfix[index] != ' ' && index != (postfix_len - 1) )
+        {
+            index++;
+            token_len++;
+        }
+
+        // operand
+        if( token[0] >= '0' && token[0] <= '9' )
+        {
+            Stack_push_uint32(stack, string_to_integer(token, token_len));
+        }
+
+        // operator
+        else if( token[0] != ' ' && token[0] != '\0' )
+        {
+            char __operator = token[0];
+            uint32_t num2 = Stack_pop_uint32(stack);
+            uint32_t num1 = Stack_pop_uint32(stack);
+
+            // Abracadabra :D
+            result = ASCII_ADDR_ARR[(uint8_t)__operator](num1, num2);
+
+            Stack_push_uint32(stack, result);
+        }
+
+        index++;
+    }
+
+    Stack_clean(stack);
+
+    return result;
+}
+
+char* remove_spaces( char* str )
+{
+    // duplicate `str`
+    char* start = strdup( str );
+
+    if( start != NULL )
+        str = start;
+
+    else
+        return NULL;
+
+    char* next  = start;
+
+    while( *next != '\0' )
+    {
+        if( *next == ' ' )
+            next++;
+
+        else
+            *(start++) = *(next++);
+    }
+
+    *start = '\0';
+
+    return str;
+}
+
+uint32_t __add( uint32_t num1, uint32_t num2 )
+{
+    return num1 + num2;
+}
+
+uint32_t __sub( uint32_t num1, uint32_t num2 )
+{
+    return num1 - num2;
+}
+
+uint32_t __mul( uint32_t num1, uint32_t num2 )
+{
+    return num1 * num2;
+}
+
+uint32_t __div( uint32_t num1, uint32_t num2 )
+{
+    return num1 / num2;
+}
+
+uint32_t __mod( uint32_t num1, uint32_t num2 )
+{
+    return num1 % num2;
+}
+
+uint32_t __shift_left( uint32_t num1, uint32_t num2 )
+{
+    return num1 << num2;
+}
+
+uint32_t __shift_right( uint32_t num1, uint32_t num2 )
+{
+    return num1 >> num2;
+}
+
+uint32_t __and( uint32_t num1, uint32_t num2 )
+{
+    return num1 & num2;
+}
+
+uint32_t __or( uint32_t num1, uint32_t num2 )
+{
+    return num1 | num2;
+}
+
+uint32_t __xor( uint32_t num1, uint32_t num2 )
+{
+    return num1 ^ num2;
 }
 
 void ExpressionParser_clean()
 {
     // clean the ascii table
-    free((char *)ASCII);
+    free((char *)ASCII_PRI);
 }
--- expression_parser.h
+++ expression_parser.h
@@ -1,3 +1,12 @@
+/* This library take a mathematical equation as STRING, interprete it
+ * and get back the answer.
+ * It could also convert infix to postfix only
+ * # USAGE:
+ * 1- call ExpressionParser_init once to initiate the library
+ * 2- call ExpressionParser_infix_to_postfix OR ExpressionParser_parse
+ * 3- call ExpressionParser_clean to clean the library
+ */
+
 #ifndef EXPRESSION_PARSER_H
 #define EXPRESSION_PARSER_H
 
@@ -8,6 +17,10 @@ void ExpressionParser_init();
 
 // convert from infix to postfix
 char* ExpressionParser_infix_to_postfix( const char* infix );
+
+// it takes the infix (the expression as string)
+// and return the result vaule of the expression back
+uint32_t ExpressionParser_parse( const char* infix );
 
 void ExpressionParser_clean();
 
